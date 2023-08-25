3c256bd15af8b0978b635953c21ec01740d00301
Jacob Padley <jacob5180@hotmail.co.uk>
2023-08-25 11:51:14 +0100

    Add URl roulette feature to /roulette

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"math/rand"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+)
+
+const (
+	MainURL     string = "https://computingsociety.co.uk"
+	SurpriseURL string = "https://www.youtube.com/watch?v=mx86-rTclzA"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/roulette", roulette)
+	r.Run("localhost:4444")
+}
+
+func roulette(c *gin.Context) {
+	n := rand.Intn(9)
+	// 1/10 chance of Rick Roll for great results
+	if n == 4 {
+		c.Redirect(http.StatusTemporaryRedirect, SurpriseURL)
+	} else {
+		c.Redirect(http.StatusTemporaryRedirect, MainURL)
+	}
 }
